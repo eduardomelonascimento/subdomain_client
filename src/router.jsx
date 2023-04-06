f39723789bee01067968ed655f6f40a42872465c
Eduardo Melo <nascimentomeloedu@gmail.com>
2023-04-06 17:10:52 -0300

    feat(router): add auth verification to routes

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Domains from "./pages/Domains";
+import Login from "./pages/Login";
 import Subdomains from "./pages/Subdomains";
 
 export default function Router() {
@@ -8,8 +9,13 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/domains" element={<Domains />} />
-        <Route path="/subdomains/:id" element={<Subdomains />} />
+        <Route path="/domains" element={<ProtectedRoute />}>
+          <Route path="/domains" element={<Domains />} />
+        </Route>
+        <Route path="/subdomains/:id" element={<ProtectedRoute />}>
+          <Route path="/subdomains/:id" element={<Subdomains />} />
+        </Route>
+        <Route path="*" element={<Navigate to={"/"}/>} />
       </Routes>
     </BrowserRouter>
   );
