7dd2f4c702a2f707b64089a2c57c8f9b81cb1cb9
Eduardo Melo <nascimentomeloedu@gmail.com>
2023-04-06 12:47:20 -0300

    feat(hooks): add the custom hook useFetch

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,26 @@
+import { useCallback, useState } from "react";
+
+export default function useFetch() {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState();
+
+  const request = useCallback(async (url, options) => {
+    let response, json;
+    try {
+      setError();
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (!response.ok) throw new Error(json.error);
+      setData(json);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return { data, error, loading, request };
+}
