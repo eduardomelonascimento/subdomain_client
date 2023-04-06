26ed249e5cb536f09cf135b02e9bf5e5acc1775b
Eduardo Melo <nascimentomeloedu@gmail.com>
2023-04-06 12:46:35 -0300

    feat(router): add routes to domains and subdomains

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Domains from "./pages/Domains";
+import Subdomains from "./pages/Subdomains";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/domains" element={<Domains />} />
+        <Route path="/subdomains/:id" element={<Subdomains />} />
       </Routes>
     </BrowserRouter>
   );
