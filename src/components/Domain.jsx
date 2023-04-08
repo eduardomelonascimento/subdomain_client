d39320fbf0f6a06ab2fca09d7490662d2ce65339
Eduardo Melo <nascimentomeloedu@gmail.com>
2023-04-08 13:08:02 -0300

    feat(components/domain): adds the feature of navigating to the subdomains by clicking on the domain URL

--- src/components/Domain.jsx
+++ src/components/Domain.jsx
@@ -1,14 +1,18 @@
-export default function Domain({id, url, handleClick}) {
+import { Link, Navigate } from "react-router-dom";
+import seta from "../assets/rightarrow_102633.svg";
+
+export default function Domain({ id, url, handleClick }) {
   return (
     <>
       <tr>
         <td className="first-col">{id}</td>
         <td>
-          <a href={url} target="_blank">
-            {url}
-          </a>
+          <Link to={`/subdomains/${id}`}>{url}</Link>
         </td>
-        <td className="action">
+        <td className="action boxcel">
+          <Link to={`/subdomains/${id}`}>
+            <img src={seta} alt="Seta apontada para a direita" />
+          </Link>
           <button title="Remover Domínio" onClick={() => handleClick(id)}>
             &#10005;
           </button>
